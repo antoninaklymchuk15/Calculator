16936630d48fcab409e086134469deb4fd3f09f5
antoninaklymchuk15 <klymchuk.antonina@gmail.com>
2023-06-16 15:16:49 +0300

    conversion to number in code has been simplified

--- index.js
+++ index.js
@@ -1,27 +1,29 @@
 let operation = prompt("Please choose operation: add, sub, mult, div ", "");
 let num1 = prompt("Enter the first number", "");
 let num2 = prompt("Enter the second number", "");
+let num1Conv = Number(num1);
+let num2Conv = Number(num2);
 let result;
 let final;
 
 switch (operation) {
   case "add":
-    result = Number(num1) + Number(num2);
+    result = num1Conv + num2Conv;
     expression = String(num1 + "+" + num2);
     break;
 
   case "sub":
-    result = Number(num1) - Number(num2);
+    result = num1Conv - num2Conv;
     expression = String(num1 + "-" + num2);
     break;
 
   case "mult":
-    result = Number(num1)*Number(num2);
+    result = num1Conv*num2Conv;
     expression = String(num1 + "*" + num2);
     break;
 
   case "div":
-    result = Number(num1)/Number(num2);
+    result = num1Conv/num2Conv;
     expression = String(num1 + "/" + num2);
     break;
 
